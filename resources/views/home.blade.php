8380b3787b6c0bfe0aea36c38886d8284915156e
Edgar Flores Torres <edgar_strm14@hotmail.com>
2021-02-17 11:30:31 -0600

    Proyecto, commit final

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+
+<div class="container py-3">
+	
+	<div class="row">
+		
+		<div class="col-12 col-sm-10 col-lg-12 mx-auto">
+			
+			<form method="POST" action="{{route('zipcode')}}" class="bg-white shadow rounded-lg py-3 px-5">
+			
+				<label>Ingresa el Código Postal</label>
+				<input type="" name="zip_code">
+							
+				<button class="btn btn-primary btn-lg btn-block" id="btsubmit" type="submit">Buscar Código Postal</button>
+			
+			</form>
+
+		</div>
+
+	</div>
+
+
+</div>
