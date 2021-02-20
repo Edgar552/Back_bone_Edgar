4977e92b8130fadf06e5bbcb025387cec9ec0416
Edgar Flores Torres <edgar_strm14@hotmail.com>
2021-02-20 09:39:23 -0600

    update controller, evitar error en backbone

---
+++ app/Http/Controllers/ZipCodesController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Http\Controllers\SimpleXMLElement;
+class ZipCodesController extends Controller
+{
+    public function show(int $id)
+   {	/*Seteo la ruta de nu archivo XML*/
+ 		$XmlPath = resource_path('xml/').'CPdescarga.xml';
+ 		/*Parseo para obtener los datos*/
+		$MyXML = simplexml_load_file ( $XmlPath , 'SimpleXMLElement' , LIBXML_NOWARNING | LIBXML_COMPACT | LIBXML_NSCLEAN    , null , false ); 
+
+		foreach ($MyXML->table as $content)
+		{
+		    if ($content->d_codigo == $id)
+		    {
+		    	echo json_encode($content);
+
+		         break;
+		    }
+
+		}
+   }
+}
